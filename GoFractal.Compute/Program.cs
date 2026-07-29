9081e58d9a358140660a14caf5e6e7b4c3005976
Isabelle Santin <isabelle@chosenfewsoftware.com>
2026-07-29 13:29:07 -0500

    Feature: finish initial implementation of fully featured app

--- GoFractal.Compute/Program.cs
+++ GoFractal.Compute/Program.cs
@@ -1,31 +1,12 @@
-﻿/*
- *  Copyright 2018-2026 Chosen Few Software
- *  This file is part of FractalSharp.
- *
- *  FractalSharp is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  FractalSharp is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with FractalSharp.  If not, see <https://www.gnu.org/licenses/>.
- */
-
+﻿
 using FractalSharp.Algorithms;
 using FractalSharp.Imaging;
 using FractalSharp.Processing;
 using GoFractal.Common;
-using QuadrupleLib;
 using SkiaSharp;
+using System.CommandLine;
 
-using TAccelerator = QuadrupleLib.Accelerators.SoftwareAccelerator;
-
-namespace FractalSharp.ExampleApp
+namespace GoFractal.Compute
 {
     unsafe class SkiaImageBuilder : UpscalingImageBuilder, IDisposable
     {
@@ -74,111 +55,148 @@ namespace FractalSharp.ExampleApp
 
     class Program
     {
-        private const int WIDTH = 2560 * 4;
-        private const int HEIGHT = 1440 * 4;
-
-        private static readonly SkiaImageBuilder Imager = new SkiaImageBuilder();
-
-        private static readonly Gradient Colors =
-            new Gradient(256, new List<GradientKey>
-            {
-                new GradientKey(new RgbaValue(0, 0, 0)),
-                new GradientKey(new RgbaValue(9, 1, 47)),
-                new GradientKey(new RgbaValue(4, 4, 73)),
-                new GradientKey(new RgbaValue(0, 7, 100)),
-                new GradientKey(new RgbaValue(12, 44, 138)),
-                new GradientKey(new RgbaValue(24, 82, 177)),
-                new GradientKey(new RgbaValue(57, 125, 209)),
-                new GradientKey(new RgbaValue(134, 181, 229)),
-                new GradientKey(new RgbaValue(211, 236, 248)),
-                new GradientKey(new RgbaValue(241, 233, 191)),
-                new GradientKey(new RgbaValue(248, 201, 95)),
-                new GradientKey(new RgbaValue(255, 170, 0)),
-                new GradientKey(new RgbaValue(204, 128, 0)),
-                new GradientKey(new RgbaValue(153, 87, 0)),
-                new GradientKey(new RgbaValue(106, 52, 3)),
-                new GradientKey(new RgbaValue(66, 30, 15)),
-                new GradientKey(new RgbaValue(25, 7, 26))
-            });
-
         private static readonly CancellationTokenSource cts = new CancellationTokenSource();
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Process started.");
-
             Console.CancelKeyPress += OnCancelKeyPress;
 
-            RenderConfig config = RenderConfig.FromJson(File.ReadAllText(args[0]));
+            RootCommand rootCommand = new RootCommand("GoFractal.Compute - Lightning fast companion app to GoFractal for high resolution, customized fractal rendering.");
 
-            IProcessor<PointData<double>> fractalProcessor = Utilities.CreateFractalProcessor(typeof(GPUFractalProcessor<,,>), config.FractalType, WIDTH, HEIGHT);
-            IProcessor<double> innerColorProcessor = Utilities.CreateColorProcessor(config.InnerColorType, WIDTH, HEIGHT);
-            IProcessor<double> outerColorProcessor = Utilities.CreateColorProcessor(config.OuterColorType, WIDTH, HEIGHT);
-
-            // touch file to allocate it (effectively lock the file)
-            using SKWStream outputStream = new SKFileWStream(Path.ChangeExtension(args[0], ".png"));
-            bool frameFinished = false;
-
-            try
+            Argument<FileInfo[]> configFilesArgument = new Argument<FileInfo[]>("config-files")
             {
-                Console.WriteLine($"Computing raw fractal data...");
-                await fractalProcessor.SetupAsync(new ProcessorConfig
-                {
-                    ThreadCount = Environment.ProcessorCount,
-                    Params = config.FractalParams,
-                }, cts.Token);
-                PointData<double>[,] inputData = await fractalProcessor.ProcessAsync(cts.Token);
+                Arity = ArgumentArity.OneOrMore,
+                Description = "List of one or more GoFractal JSON files to render"
+            };
+            configFilesArgument.AcceptExistingOnly();
+            rootCommand.Add(configFilesArgument);
 
-                Console.WriteLine("Computing colors for inner points...");
-                await innerColorProcessor.SetupAsync(new ColorProcessorConfig
-                {
-                    ThreadCount = Environment.ProcessorCount,
-
-                    Params = config.InnerColorParams,
-                    PointClass = PointClass.Inner,
-
-                    InputData = inputData
-                }, cts.Token);
-                double[,] innerIndicies = await innerColorProcessor.ProcessAsync(cts.Token);
-
-                Console.WriteLine("Computing colors for outer points...");
-                await outerColorProcessor.SetupAsync(new ColorProcessorConfig
-                {
-                    ThreadCount = Environment.ProcessorCount,
-
-                    Params = config.OuterColorParams,
-                    PointClass = PointClass.Outer,
-
-                    InputData = inputData
-                }, cts.Token);
-                double[,] outerIndicies = await outerColorProcessor.ProcessAsync(cts.Token);
-
-                Console.WriteLine("Building image...");
-                Imager.CreateImage(outerIndicies, innerIndicies, Colors, Colors);
-
-                Console.WriteLine("Writing image file to disk...");
-                Imager.Bitmap?.Encode(outputStream, SKEncodedImageFormat.Png, 100);
-
-                Console.WriteLine("Image rendered successfully!");
-                frameFinished = true;
-            }
-            catch (AggregateException ex) when (ex.InnerExceptions.Any(err => err.GetType() == typeof(OperationCanceledException))) { }
-            catch (OperationCanceledException) { }
-            cts.Dispose();
-
-            if (!frameFinished)
+            Option<int> imageWidthOption = new Option<int>("--image-width")
             {
-                File.Delete(Path.ChangeExtension(args[0], ".png"));
-            }
+                DefaultValueFactory = r => 3840,
+                Description = "Width of the output image in pixels"
+            };
+            rootCommand.Add(imageWidthOption);
 
-            Imager.Dispose();
+            Option<int> imageHeightOption = new Option<int>("--image-height")
+            {
+                DefaultValueFactory = r => 2160,
+                Description = "Height of the output image in pixels"
+            };
+            rootCommand.Add(imageHeightOption);
 
-            Console.WriteLine("Process halted. All remaining tasks completed gracefully!");
+            Option<int> numThreadsOption = new Option<int>("--num-threads")
+            {
+                DefaultValueFactory = r => Environment.ProcessorCount,
+                Description = "Number of threads to use for image rendering"
+            };
+            rootCommand.Add(numThreadsOption);
+
+            rootCommand.SetAction(async Task (r, ct) =>
+            {
+                Console.WriteLine("Process started.");
+
+                foreach (string configFilePath in r.GetRequiredValue(configFilesArgument).Select(x => x.FullName))
+                {
+                    if (ct.IsCancellationRequested) break;
+
+                    string imageFilePath = Path.ChangeExtension(configFilePath, ".png");
+
+                    int imageWidth = r.GetRequiredValue(imageWidthOption);
+                    int imageHeight = r.GetRequiredValue(imageHeightOption);
+
+                    int numThreads = r.GetRequiredValue(numThreadsOption);
+
+                    bool frameFinished = false;
+                    try
+                    {
+                        RenderConfig config = RenderConfig.FromJson(File.ReadAllText(configFilePath));
+                        Console.WriteLine($"Found configuration named \"{config.Name}\" in \"{Path.GetFileName(configFilePath)}\".");
+
+                        Console.WriteLine($"Computing raw fractal data...");
+                        IProcessor<PointData<double>> fractalProcessor = Utilities.CreateFractalProcessor(typeof(GPUFractalProcessor<,,>), config.FractalType, imageWidth, imageHeight);
+
+                        PointData<double>[,] inputData;
+                        using (fractalProcessor as IDisposable)
+                        {
+                            await fractalProcessor.SetupAsync(new ProcessorConfig
+                            {
+                                ThreadCount = numThreads,
+                                Params = config.FractalParams,
+                            }, ct);
+                            inputData = await fractalProcessor.ProcessAsync(ct);
+                        }
+
+                        Console.WriteLine("Computing colors for inner points...");
+                        IProcessor<double> innerColorProcessor = Utilities.CreateColorProcessor(config.InnerColorType, imageWidth, imageHeight);
+
+                        double[,] innerIndicies;
+                        using (innerColorProcessor as IDisposable)
+                        {
+                            await innerColorProcessor.SetupAsync(new ColorProcessorConfig
+                            {
+                                ThreadCount = numThreads,
+                                Params = config.InnerColorParams,
+
+                                PointClass = PointClass.Inner,
+                                InputData = inputData
+                            }, ct);
+                            innerIndicies = await innerColorProcessor.ProcessAsync(ct);
+                        }
+
+                        Console.WriteLine("Computing colors for outer points...");
+                        IProcessor<double> outerColorProcessor = Utilities.CreateColorProcessor(config.OuterColorType, imageWidth, imageHeight);
+
+                        double[,] outerIndicies;
+                        using (outerColorProcessor as IDisposable)
+                        {
+                            await outerColorProcessor.SetupAsync(new ColorProcessorConfig
+                            {
+                                ThreadCount = numThreads,
+                                Params = config.OuterColorParams,
+
+                                PointClass = PointClass.Outer,
+                                InputData = inputData
+                            }, ct);
+                            outerIndicies = await outerColorProcessor.ProcessAsync(ct);
+                        }
+
+                        Console.WriteLine("Building image...");
+
+                        SKBitmap bitmap;
+                        using (SkiaImageBuilder imager = new SkiaImageBuilder())
+                        {
+                            imager.CreateImage(outerIndicies, innerIndicies, config.OuterGradient, config.InnerGradient);
+                            bitmap = imager.Bitmap!.Copy();
+                        }
+
+                        Console.WriteLine($"Writing \"{Path.GetFileName(imageFilePath)}\" next to JSON configuration file...");
+                        using (SKWStream outputStream = new SKFileWStream(imageFilePath))
+                        {
+                            bitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+                        }
+
+                        Console.WriteLine("Image rendered successfully!");
+                        frameFinished = true;
+                    }
+                    catch (AggregateException ex) when (ex.InnerExceptions.Any(err => err.GetType() == typeof(OperationCanceledException))) { }
+                    catch (OperationCanceledException) { }
+
+                    if (!frameFinished)
+                    {
+                        File.Delete(imageFilePath);
+                    }
+                }
+
+                Console.WriteLine("Process halted. All remaining tasks completed gracefully!");
+            });
+
+            await rootCommand.Parse(args).InvokeAsync(cancellationToken: cts.Token);
         }
 
         private static void OnCancelKeyPress(object? sender, ConsoleCancelEventArgs e)
         {
-            Console.WriteLine("Cancellation registered! Waiting for current processing to complete...");
+            Console.Error.WriteLine("Cancellation registered! Waiting for current processing to complete...");
             cts.Cancel();
             e.Cancel = true;
         }
