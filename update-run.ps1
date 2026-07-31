b99c297f12954982ade28131ef2758b0a41e2d9b
Isabelle Santin <isabelle@chosenfewsoftware.com>
2026-07-31 11:18:18 -0500

    Feature: implement launcher scripts for better usability

---
+++ update-run.ps1
@@ -0,0 +1,6 @@
+# Pull latest sources
+git pull
+git submodule update --init --recursive
+
+# Run application
+dotnet run --project GoFractal.Compute\GoFractal.Compute.csproj --configuration Release -- $args
